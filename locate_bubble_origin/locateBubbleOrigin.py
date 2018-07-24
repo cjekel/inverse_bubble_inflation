a81a244ccd7448997429b80f06b9b09ac18973e1
a-bernardo <andresbernardo@comcast.net>
2018-07-24 15:15:53 -0400

    Updates to locateBubbleOrigin.py Z-polyfit was found to be not very useful for low-pressure data; in the process of coding, a median-method was introduced as a replacement. The auxiliary file for looping locateBubbleOrigin.py was integrated. The correctZ method was removed from locateBubbleOrigin.py and comments were added to the file; correctZ may be used in the future for "origin correction" (Outputted plots to follow in next commits, pending axes limits considerations)

--- locate_bubble_origin/locateBubbleOrigin.py
+++ locate_bubble_origin/locateBubbleOrigin.py
@@ -35,9 +35,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-#plt.ioff()
-
-def locateBubbleOrigin(dicFileInput, plotFileName):
+def locateBubbleOrigin(dicFileInput, plotFilePath2D, plotFilePath3D):
+    
+    ## Note: code for data point annotations have been commented out,
+    ## but were left within code for future possible improvements
     
     ##=========================================================================
     ## utilizing np.loadtxt  
@@ -51,9 +52,9 @@ def locateBubbleOrigin(dicFileInput, plotFileName):
     ##=========================================================================
     values = values[ (values[:,2] != 0) | (values[:,5] != 0) ]
     
-    print ('\nThere are')
-    print (len(values))
-    print ('data entries when zero-displacement points are removed')
+#    print ('\nThere are')
+#    print (len(values))
+#    print ('data entries when zero-displacement points are removed')
     
     X = values[:,0]
     Y = values[:,1]
@@ -77,9 +78,6 @@ def locateBubbleOrigin(dicFileInput, plotFileName):
     yMin = min(finalY)
     yMax = max(finalY)
     
-    # find the maximum value in the z-direction
-    #    zMax = np.max(finalZ)
-    
     # find the x & y values associated with each extreme
     y_coordinate_of_xMax = finalY[ finalX.argmax() ]
     y_coordinate_of_xMin = finalY[ finalX.argmin() ]
@@ -90,16 +88,15 @@ def locateBubbleOrigin(dicFileInput, plotFileName):
     x_coordinate_of_zMax = finalX[ finalZ.argmax() ]
     y_coordinate_of_zMax = finalY[ finalZ.argmax() ]
     
+    
     #==========================================================================
     # 2D plot
-    #==========================================================================
-    
+    #==========================================================================    
     fig2D = plt.figure()
-    ax2D = fig2D.add_subplot(111)
-    
+    ax2D = fig2D.add_subplot(111)    
     map2D = ax2D.scatter(finalX, finalY, c=finalZ, s=8, edgecolor='')
     
-    # include the colorbar
+    # include the colorbar to illustrate Z-values in 2D plot
     plt.colorbar(map2D, ax=ax2D, format='%.0f mm')
     
     # label axes
@@ -114,8 +111,8 @@ def locateBubbleOrigin(dicFileInput, plotFileName):
     ax2D.axhline(0, color='k', lw=0.5, ls='dashed')
     ax2D.axvline(0, color='k', lw=0.5, ls='dashed')
     
-    # annotate origin [x = 0 mm, y = 0 mm]
-    #plt.scatter(0, 0, c='k', s=10, edgecolor='')
+    ## annotate origin [x = 0 mm, y = 0 mm]
+#    plt.scatter(0, 0, c='k', s=10, edgecolor='')
 #    ax2D.annotate('(x=0, y=0)',
 #                 size=7,
 #                 xy = [0, 0],
@@ -124,8 +121,8 @@ def locateBubbleOrigin(dicFileInput, plotFileName):
     
     # plot & annotate maximum Z value
     ax2D.scatter(x_coordinate_of_zMax, y_coordinate_of_zMax, \
-                        s=50, marker='s', facecolors='none', edgecolors='k', \
-                        label='Z$_{max}$ = (%.2f, %.2f) mm' \
+                        s=100, marker='s', facecolors='none', edgecolors='k', \
+                        label='Z$_{raw Max}$ = (%.2f, %.2f) mm' \
                         %(x_coordinate_of_zMax, y_coordinate_of_zMax ))
 #    ax2D.annotate('Z$_{max}$', 
 #                 size=7,
@@ -173,9 +170,7 @@ def locateBubbleOrigin(dicFileInput, plotFileName):
                  c='r', s=25, \
                  label='Z$_{intersect}$ = (%.2f, %.2f) mm' \
                      %(x_intersect, y_intersect))
-    
-#    intersect_coordinate_str = '(%.2f, %.2f)' %(x_intersect, y_intersect)
-#    ax2D.annotate(intersect_coordinate_str, 
+#    ax2D.annotate('(%.2f, %.2f)' %(x_intersect, y_intersect), 
 #                 size=7,
 #                 color='k',
 #                 xy = [x_intersect, y_intersect],
@@ -185,20 +180,18 @@ def locateBubbleOrigin(dicFileInput, plotFileName):
     
     ##=========================================================================
     ## 3D plot (plot of the surfaces fits of Disp Z)
-    ##=========================================================================
-    
+    ##=========================================================================    
     from doubleVander import doubleVander
     from polySurfaceFit import polySurfaceFit
-    from correctZ import correctZ
-    
-    newFinalX, newFinalY, newFinalZ, x_center, y_center = \
-        correctZ(X, Y, Z, dispX, dispY, dispZ)
+    from circleFit import circleFit
+
+    x_center, y_center, radius = circleFit(finalX, finalY)
     
     cDispZ, rDispZ, fittedDispZ = \
-        polySurfaceFit(newFinalX,newFinalY,newFinalZ, 4)
+        polySurfaceFit(finalX, finalY, finalZ, 4)
     
-    X,Y = np.meshgrid(np.linspace(min(newFinalX), max(newFinalX), 20), \
-                      np.linspace(min(newFinalY), max(newFinalY), 20))
+    X,Y = np.meshgrid(np.linspace(min(finalX), max(finalX), 50), \
+                      np.linspace(min(finalY), max(finalY), 50))
     
     Axy = doubleVander(X.flatten(),Y.flatten(),4)
     
@@ -211,67 +204,76 @@ def locateBubbleOrigin(dicFileInput, plotFileName):
     
     ax3D.plot_surface(X, Y, Z, rstride=1, cstride=1, alpha=0.2)
     
-    ax3D.scatter(newFinalX, newFinalY, newFinalZ, zdir='z', \
+    ax3D.scatter(finalX, finalY, finalZ, zdir='z', \
                  s=.2,c='b',edgecolor='', depthshade=False)
     
     ax3D.set_aspect('equal')
-    ax3D.view_init(0, 90)
-    
-    if len(values) > 0: 
-         xMin = np.round( np.min(finalX) ); xMax = np.round( np.max(finalX) ) 
-         yMin = np.round( np.min(finalY) ); yMax = np.round( np.max(finalY) ) 
-    #     zMin = np.round( np.min(finalZ) );
-         zMax = np.round( np.max(finalZ) ) 
-         ax3D.set_xlim3d( xMin-5, xMax+5 ) 
-         ax3D.set_ylim3d( yMin-5, yMax+5 )
-         ax3D.set_zlim3d(-100, zMax+5)
-    else:
-        ax3D.set_xlim3d(-100, 100)
-        ax3D.set_ylim3d(-100,100)
-        ax3D.set_zlim3d(-100,100)
      
     ax3D.set_xlabel('X (mm)')
     ax3D.set_ylabel('Y (mm)')
     ax3D.set_zlabel('Z (mm)')
     
-    ax3D.set_title('3D plot of Disp Z over the XY bubble position')
+    ax3D.set_title('3D plot of Disp Z over the XY bubble position' + \
+                   '\n(with polynomial fit)')
     
-    
-    #==========================================================================
     #==========================================================================
     # Marking the highest Z from the fitted polynomial on 2D plot
+    #
+    # (This method has been commented out; for middle/high pressure data,
+    # the max Z value of the polyfit was useful, because the curve of the   
+    # polyfit was primarily "parabolic", so Z_polyfit would be at the
+    # "top of the dome". However, low-pressure data resulted in curves  
+    # that were primarily "hyperbolic", which lead to Z_polyfit lying upon a
+    # "corner" of the resulting curve.) 
     #==========================================================================
-    
-    x_coord, y_coord = np.unravel_index(Z.argmax(), Z.shape)
-    
-    ax3D.scatter(X[x_coord,y_coord], Y[x_coord,y_coord], \
-               c='k', s=100, edgecolor='', marker='+')
-    
-    ax2D.scatter(X[x_coord,y_coord], Y[x_coord,y_coord], \
-                 c='k', s=100, edgecolor='', marker='+', \
-                 label='Z$_{polyfit}$ = (%.2f, %.2f) mm' \
-                     %(X[x_coord,y_coord], Y[x_coord,y_coord]))
-   
-#    ax2D.annotate('Z$_{polyfit}$ = (%.2f, %.2f)' \
-#                      %(X[x_coord,y_coord], Y[x_coord,y_coord]), \
-#                 size=7,
-#                 xy = [X[x_coord,y_coord], Y[x_coord,y_coord]],
-#                 xytext=(-20, 15),
-#                 textcoords = 'offset pixels',
-#                 arrowprops=dict(facecolor='black', arrowstyle='->'))
-    
-    
-    #==========================================================================
+#    x_polyfit, y_polyfit = np.unravel_index(Z.argmax(), Z.shape)   
+#        
+#    ax3D.scatter(X[x_polyfit, y_polyfit], \
+#                 Y[x_polyfit, y_polyfit], \
+#                 Z[x_polyfit, y_polyfit], \
+#                 c='k', s=100, edgecolor='', marker='+')
+#    
+#    ax2D.scatter(X[x_polyfit, y_polyfit], \
+#                 Y[x_polyfit, y_polyfit], \
+#                 c='k', s=100, edgecolor='', marker='+', \
+#                 label='Z$_{polyfit}$ = (%.2f, %.2f) mm' \
+#                     %(X[x_polyfit,y_polyfit], Y[x_polyfit,y_polyfit]))
+##    ax2D.annotate('Z$_{polyfit}$ = (%.2f, %.2f)' \
+##                      %(X[x_coord,y_coord], Y[x_coord,y_coord]), \
+##                 size=7,
+##                 xy = [X[x_coord,y_coord], Y[x_coord,y_coord]],
+##                 xytext=(-20, 15),
+##                 textcoords = 'offset pixels',
+##                 arrowprops=dict(facecolor='black', arrowstyle='->'))
     
     #==========================================================================
-    # Marking center derived from circleFit function within correctZ
-    #==========================================================================
+    # Marking the median X & median Y from the fitted polynomial on 2D plot
+    #==========================================================================   
+    x_median = np.median(X)
+    y_median = np.median(Y)
     
+    ax2D.scatter(x_median, \
+                 y_median, \
+                 c='k', s=200, edgecolor='', marker='+', \
+                 label='Z$_{poly XY median}$ = (%.2f, %.2f) mm' \
+                     %(x_median, y_median))   
+    
+    # perhaps we can look at the median from the raw data as well??
+    x_median = np.median(finalX)
+    y_median = np.median(finalY)    
+    ax2D.scatter(x_median, \
+                 y_median, \
+                 s=200, edgecolors='k', marker='^', facecolors='none', \
+                 label='Z$_{raw XY median}$ = (%.2f, %.2f) mm' \
+                     %(x_median, y_median)) 
+    
+    #==========================================================================
+    # Marking center derived from circleFit function
+    #==========================================================================
     ax2D.scatter(x_center, y_center, \
-                 s=75, marker='o', facecolors='none', edgecolors='k', \
+                 s=200, marker='o', facecolors='none', edgecolors='k', \
                  label='Z$_{circlefit}$ = (%.2f, %.2f) mm' \
                      %(x_center, y_center))
-    
 #    ax2D.annotate('Z$_{circlefit}$ = (%.2f, %.2f)' %(x_center, y_center), 
 #                 size=7,
 #                 xy = [x_center, y_center],
@@ -280,37 +282,82 @@ def locateBubbleOrigin(dicFileInput, plotFileName):
 #                 arrowprops=dict(facecolor='white', arrowstyle='->'))
     
     #==========================================================================
-    # Resize 2D figure and show plots 
+    # Resize 2D figure, generate legend, and show and/or save plots 
     #==========================================================================
-    
-    fig2D.set_size_inches(np.array([10,8]))
+    fig2D.set_size_inches(np.array([12,12]))
     ax2D.set_aspect('equal')
+    ax2D.xaxis.set_ticks(np.arange(-100, 125, 25))
+    ax2D.yaxis.set_ticks(np.arange(-100, 125, 25))
     
-    ax2D.legend(fancybox=True, fontsize='small')
-#    fig2D.legend([zMax,intersect,zPolyfit,zCircleFit], \
-#               ['Z$_{max}$ = (%.2f, %.2f) mm' \
-#                    %(x_coordinate_of_zMax, y_coordinate_of_zMax), \
-#                'Z$_{intersect}$' = (%.2f, %.2f) mm' \
-#                    %(x_intersect, y_intersect), \
-#                'Z$_{polyfit}$ = (%.2f, %.2f) mm' \
-#                    %(X[x_coord,y_coord], Y[x_coord,y_coord]),
-#                'Z$_{circlefit}$ = (%.2f, %.2f) mm' \
-#                    %(x_center, y_center)])
-#    plt.legend()
+    ax2D.legend(fancybox=True, fontsize='small', loc='lower right', \
+                markerscale=0.5)
     
-    
-    fig2D.show()
-    
-#    fig2D.savefig(plotFileName, bbox_inches='tight', dpi=100)
-#    plt.close()
+#    fig2D.show()    
+    fig2D.savefig(plotFilePath2D, bbox_inches='tight', dpi=200)
+    plt.close(fig2D)
 
-    fig3D.show()
-
+    fig3D.set_size_inches(np.array([8,8]))
+#    fig3D.show()
+    fig3D.savefig(plotFilePath3D, bbox_inches='tight', dpi=200)
+    plt.close(fig3D)
 
 ##=============================================================================
+
+###############################################################################
     
-locateBubbleOrigin('C:\\temp\demo\B00015.dat','C:\\temp\demo_plot\B00015.png')
-#locateBubbleOrigin('C:\\temp\Valmex_BubbleTest00\TecData\B00012.dat', \
-#                   'C:\\temp\demo_plot\B00012.png')
-#locateBubbleOrigin('C:\\temp\Valmex_BubbleTest00\TecData\B00001.dat', \
-#                   'C:\\temp\demo_plot2\B00001.png')
+# =============================================================================
+# locatebubbleOrigin.py loop
+# =============================================================================
+
+plt.ioff()
+## used when running locateBubbleOrigin.py in the following loop;
+## this will help suppress figure output (see plt.close() commands above)
+
+import os
+import os.path as path
+import glob
+
+datFolderNameList = \
+    [ \
+    'C:\\temp\Blue_PVC_Valmex_BubbleTest00', \
+    'C:\\temp\Blue_PVC_Valmex_BubbleTest01', \
+    'C:\\temp\Blue_PVC_Valmex_BubbleTest02', \
+    'C:\\temp\Blue_PVC_Valmex_BubbleTest03', \
+    'C:\\temp\Black_PVC_Cape_Coaters_BubbleTest01\\' + \
+        'BubbleTest01_Strain_LSM_TS (27x27 stepsize=7)', \
+    'C:\\temp\Black_PVC_Cape_Coaters_BubbleTest02\\' + \
+        'BubbleTest02_Strain_LSM_TS (27x27 stepsize=7)', \
+    'C:\\temp\Black_PVC_Cape_Coaters_BubbleTest03\\' + \
+        'BubbleTest03_Strain_LSM_TS (27x27 stepsize=7)', \
+    'C:\\temp\Black_PVC_Cape_Coaters_BubbleTest04\\' + \
+        'BubbleTest04_Strain_LSM_TS (27x27 stepsize=7)', \
+    ]
+
+for line in datFolderNameList :
+
+    baseName = path.basename(line)
+    plotFolderName2D = baseName + '_origin2Dplots'
+    plotFolderPath2D = path.normpath(path.join('C:\\temp', plotFolderName2D))
+    plotFolderName3D = baseName + '_poly3Dplots'
+    plotFolderPath3D = path.normpath(path.join('C:\\temp', plotFolderName3D)) 
+       
+    os.makedirs(plotFolderPath2D)
+    os.makedirs(plotFolderPath3D)
+
+    if 'Valmex' in line:
+        line = path.join(line, '*TecData*\*.dat')
+    elif 'Cape_Coaters' in line:
+        line = path.join(line, '*.dat')
+    
+    datList = glob.glob(line)    
+    
+    for line in datList:
+        plotFilePath = path.splitext(path.basename(line))[0]
+        plotFilePath = baseName + '_' + plotFilePath
+        plotFilePath2D = plotFilePath + '_origin2Dplot.png'
+        plotFilePath2D = path.join(plotFolderPath2D, plotFilePath2D)
+        plotFilePath3D = plotFilePath + '_poly3Dplot.png'
+        plotFilePath3D = path.join(plotFolderPath3D, plotFilePath3D)
+        locateBubbleOrigin(line, plotFilePath2D, plotFilePath3D)
+#        print(line, '\n', plotFilePath2D, '\n', plotFilePath3D)
+#    print()
